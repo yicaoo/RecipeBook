0075469ce81ef1fa1a7ac8d898016c58dd3f6724
yicaoo <caoy1028@gmail.com>
2018-11-13 12:44:00 -0800

    Initial Commit

---
+++ RecipeBook/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RecipeBook
+//
+//  Created by Yi Cao on 11/13/18.
+//  Copyright © 2018 Yi Cao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
