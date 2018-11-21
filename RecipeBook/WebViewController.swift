7c16a03e48fd673bea4edfb99244be45ae9203df
yicaoo <caoy1028@gmail.com>
2018-11-21 14:00:58 -0800

    progress bar added

--- RecipeBook/WebViewController.swift
+++ RecipeBook/WebViewController.swift
@@ -13,9 +13,13 @@ class WebViewController: UIViewController, WKUIDelegate {
     
     var webView: WKWebView!
     var pageURL: String!
+    var progressView: UIProgressView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        progressView = UIProgressView(progressViewStyle: .bar)
+       view.addSubview(progressView)
+        setupProgressView()
         // setup page request
         if let url = URL(string: pageURL) {
             let urlRequest: URLRequest = URLRequest(url: url)
@@ -28,6 +32,28 @@ class WebViewController: UIViewController, WKUIDelegate {
         let webConfiguration = WKWebViewConfiguration()
         webView = WKWebView(frame: .zero, configuration: webConfiguration)
         webView.uiDelegate = self
-        view = webView // always assigned to fill the window 
+        view = webView // always assigned to fill the window
+        
+    }
+    
+    // observe oading value changes
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        if keyPath == StringConstants.progressKeypath {
+            progressView.progress = Float(webView.estimatedProgress)
+        }
+    }
+    
+    private func setupProgressView() {
+        progressView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            progressView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            progressView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            progressView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+            ])
+        webView.addObserver(self, forKeyPath: #keyPath(WKWebView.estimatedProgress), options: .new, context: nil)
+    }
+    
+    private struct StringConstants {
+        static let progressKeypath = "estimatedProgress"
     }
 }
